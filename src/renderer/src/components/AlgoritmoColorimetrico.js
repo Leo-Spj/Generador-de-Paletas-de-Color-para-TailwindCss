aa0e6b450f096d8bab17f2ecf1febfb7c04d850f
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-05-13 16:29:44 -0500

    función de generación de gama de colores en AlgoritmoColorimetrico.js

--- src/renderer/src/components/AlgoritmoColorimetrico.js
+++ src/renderer/src/components/AlgoritmoColorimetrico.js
@@ -1,13 +1,20 @@
-function generarGamaDeColores(colorBase, cantidad) {
-  let [r, g, b] = colorBase.match(/\w\w/g).map(x => parseInt(x, 16));
+function generarGamaDeColores(colorHex, cantidad) {
 
-  let pasos = Array.from({length: cantidad}, (_, i) => {
-    let factor = i / (cantidad - 1) * 0.9;
-    let rNuevo = Math.round(r + factor * (255 - r)).toString(16).padStart(2, '0');
-    let gNuevo = Math.round(g + factor * (255 - g)).toString(16).padStart(2, '0');
-    let bNuevo = Math.round(b + factor * (255 - b)).toString(16).padStart(2, '0');
-    return rNuevo + gNuevo + bNuevo;
-  });
+  const r = parseInt(colorHex.substr(1, 2), 16); // el 16 es la base, por tanto al hacer el parseint se convierte a decimal
+  const g = parseInt(colorHex.substr(3, 2), 16);
+  const b = parseInt(colorHex.substr(5, 2), 16);
+
+  let pasos = [];
+  for (let i = 0; i < cantidad; i++) {
+      let factor = i / (cantidad - 1) * 0.9;
+      let rNuevo = Math.round(r + factor * (255 - r));
+      let gNuevo = Math.round(g + factor * (255 - g));
+      let bNuevo = Math.round(b + factor * (255 - b));
+      rNuevo = rNuevo.toString(16).padStart(2, '0');
+      gNuevo = gNuevo.toString(16).padStart(2, '0');
+      bNuevo = bNuevo.toString(16).padStart(2, '0');
+      pasos.push(rNuevo + gNuevo + bNuevo);
+  }
 
   return pasos.reverse();
 }
